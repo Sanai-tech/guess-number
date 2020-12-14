6f984dc167df9c6803b114499bc1c9aee0ddae23
Sanai-tech <gdandbssanai@gmail.com>
2020-12-14 15:17:26 +0300

    Final Version

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let compNum
 let hint = document.querySelector(".hint")
+let tries
 document.querySelector(".new_game").onclick=function () {
 	compNum=Math.ceil(Math.random()*10)
 	console.log(compNum)
 	hint.innerHTML=""
+	tries=3
+	disableButtons(false)
+	document.querySelector(".attempt").innerHTML='Attempt: '+tries
 }
 document.querySelector(".buttons").onclick=function(){
 	if (event.target.className=="number") {
@@ -13,27 +17,28 @@ document.querySelector(".buttons").onclick=function(){
 		}
 		if (myNum>compNum) {
 			hint.innerHTML="NO.Number is lower."
+			tries--
 		}
 		if (myNum<compNum) {
 			hint.innerHTML="NO.Number is bigger."
+			tries--
+
 		}
+		if (tries<1) {
+		disableButtons(true)
+
 	}
+	document.querySelector(".attempt").innerHTML='Attempt: '+tries
+}
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// когда мы нажмем нью гейм,то компьютер загадывает число
-// когда мы нажмем на цифру,то надо сравнить цифру компьютера с нашей
+function disableButtons(disabled){
+	let allButtons=document.querySelectorAll('.number')
+	for (var i = 0; i < allButtons.length; i++) {
+		allButtons[i].disabled=disabled
+	}
+	
+}
